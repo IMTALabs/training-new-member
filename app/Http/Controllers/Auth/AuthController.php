d2a651ff2398f5027566d9ba985f999db62d552f
vanvan9825 <tvan19525@gmail.com>
2025-04-22 21:16:30 +0700

    feat: Register function

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -60,5 +63,55 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect('/login');
+    }
+    public function showRegisterForm()
+    {
+        return view('auth.register');
+    }
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8) // độ dài tối thiểu
+                    ->mixedCase() // chữ hoa, chữ thường
+                    ->letters() // chứa chữ
+                    ->numbers() // chứa số
+                    ->symbols() // chứa ký tự đặc biệt
+            ],
+            'date_of_birth' => 'nullable|date',
+            'gender' => 'nullable|string',
+            'phone_number' => 'nullable|string',
+            'address' => 'nullable|string'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'avatar' => null,
+            'date_of_birth' => $request->date_of_birth,
+            'gender' => $request->gender,
+            'phone_number' => $request->phone_number,
+            'address' => $request->address,
+            'role' => $request->role ?? 'user',
+            'status' => '1',
+        ]);
+        //  dd($user);
+        Auth::attempt([
+            'email' => $user->email,
+            'password' => $user->password,
+
+        ]);
+        // dd($user);
+        $key = 'login-attempts:' . $request->ip();
+        RateLimiter::clear($key); // Xóa đếm khi đăng nhập thành công
+        $request->session()->regenerate(); // Bảo mật session
+        return redirect()->intended('/');
+        // dd(Auth::user()->name);
+
     }
 }
