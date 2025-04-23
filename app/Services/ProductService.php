5e82adf970266f4f8c2f6885d4b360dcfc1f5dc4
HảiLamdz <vuhailam2112@gmail.com>
2025-04-23 16:39:23 +0700

    refactor: remove unnecessary !empty() checks

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -8,22 +8,22 @@ class ProductService{
     public function getAllProduct($request) {
         $query = Product::with('category');
 
-        if (!empty($request->filled('name'))) {
+        if ($request->filled('name')) {
             $query->where('name', 'like', '%' . $request->name . '%');
         }
     
-        if (!empty($request->filled('category'))) {
+        if ($request->filled('category')) {
             $query->where('category_id', $request->category);
         }
     
-        if (!empty($request->filled('status'))) {
+        if ($request->filled('status')) {
             $query->where('status', $request->status);
         }
     
-        if (!empty($request->filled('sort'))) {
+        if ($request->filled('sort')) {
             $query->orderBy('price', $request->sort);
         }
-
+    
         return $query->orderBy('created_at', 'desc')->paginate(10);
     }
 
