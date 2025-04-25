cef901a3819a285b7fe7169582f250a016fba8e8
sky-quanph <quanph@skylab.vn>
2025-04-25 11:31:34 +0700

    feat: product list

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -2,35 +2,94 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Models\Category;
-use Illuminate\Http\Request;
-use App\Services\ProductService;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
 
 class ProductController extends Controller
 {
-    protected $productService;
 
-    public function __construct(ProductService $productService)
-    {
-        $this->productService = $productService;
-    }
 
-    public function index(Request $request)
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
     {
-        $products = $this->productService->getAllProduct($request);
         $categories = Category::all();
-        return view('admin.products.listProduct', compact('products', 'categories'));
+        return view('admin.products.create', compact('categories'));
     }
 
-    public function destroy($id)
-    {
-        $result = $this->productService->deleteProduct($id);
+    /**
+     * Store a newly created resource in storage.
+     */
 
-        if ($result) {
-            return redirect()->back()->with('success', 'Product delete successfully.');
-        } else {
-            return redirect()->back()->with('error', 'Product delete failed.');
+    public function store(StoreProductRequest $request)
+    {
+        $validated = $request->validated();
+
+        // Xử lý upload ảnh
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('products', 'public');
+            $validated['image'] = $imagePath;
         }
+
+        Product::create($validated);
+
+        return redirect()->route('admin.products.index')->with('success', 'Thêm sản phẩm thành công!');
+    }
+
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $product = Product::findOrFail($id);
+        $categories = Category::all();
+        return view('admin.products.edit', compact('product', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, string $id)
+    {
+        $product = Product::findOrFail($id);
+        
+        // Validate dữ liệu
+        $validated = $request->validated();
+
+        $product->fill($validated);
+        // Cập nhật thông tin
+        $product->name           = $request->name;
+        $product->category_id    = $request->category_id;
+        $product->price          = $request->price;
+        $product->import_price   = $request->import_price;
+        $product->discount_price = $request->discount_price;
+        $product->quantity       = $request->quantity;
+        $product->status         = $request->status;
+        $product->description    = $request->description;
+
+        // Xử lý ảnh nếu có upload mới
+        if ($request->hasFile('image')) {
+            // Xóa ảnh cũ nếu tồn tại
+            if ($product->image && Storage::exists('public/' . $product->image)) {
+                Storage::delete('public/' . $product->image);
+            }
+
+            // Lưu ảnh mới
+            $imagePath = $request->file('image')->store('products', 'public');
+            $product->image = $imagePath;
+        }
+
+        $product->save();
+
+        return redirect()->route('admin.products.index')->with('success', 'Cập nhật sản phẩm thành công!');
     }
 }
